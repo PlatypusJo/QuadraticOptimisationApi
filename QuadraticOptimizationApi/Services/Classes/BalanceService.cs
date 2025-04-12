2e18e9bc72bf748d92ac1a52035c44b087bbe1c5
PlatypusJo <maxim_muravyov@mail.ru>
2025-04-12 14:05:04 +0300

    Add services and Nuget packages

---
+++ QuadraticOptimizationApi/Services/Classes/BalanceService.cs
@@ -0,0 +1,48 @@
+﻿using QuadraticOptimizationApi.Converters;
+using QuadraticOptimizationApi.RequestModels;
+using QuadraticOptimizationApi.ResponseModels;
+using QuadraticOptimizationApi.Services.Interfaces;
+using QuadraticOptimizationSolver.Interfaces;
+using QuadraticOptimizationSolver.Solvers;
+
+namespace QuadraticOptimizationApi.Services.Classes
+{
+    public class BalanceService : IBalanceService
+    {
+        #region Fields
+
+        BalanceSolver _solver;
+
+        #endregion
+
+        #region Constructors
+
+        public BalanceService()
+        {
+            _solver = new BalanceSolver();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public BalanceResponse Solve(BalanceRequest request)
+        {
+            var dataModel = BalanceDataModelConverter.Convert(request);
+            var result = _solver.Solve(dataModel);
+
+            var response = new BalanceResponse
+            {
+                BalancedFlows = request.Flows
+                .Select((f, i) => new BalancedFlow { Name = f.Name, Value = result[i] })
+                .ToList(),
+                IsBalanced = true
+            };
+
+            return response;
+        }
+
+        #endregion
+        
+    }
+}
---
+++ QuadraticOptimizationApi/Services/Interfaces/IBalanceService.cs
@@ -0,0 +1,10 @@
+﻿using QuadraticOptimizationApi.RequestModels;
+using QuadraticOptimizationApi.ResponseModels;
+
+namespace QuadraticOptimizationApi.Services.Interfaces
+{
+    public interface IBalanceService
+    {
+        BalanceResponse Solve(BalanceRequest request);
+    }
+}
